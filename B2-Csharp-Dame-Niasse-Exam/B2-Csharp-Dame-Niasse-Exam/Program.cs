34649faa3c426b23ac9fa8d6759c97ca06c820b6
dame0112 <58723018+dame0112@users.noreply.github.com>
2020-10-05 13:43:37 +0200

    premier commit

---
+++ B2-Csharp-Dame-Niasse-Exam/B2-Csharp-Dame-Niasse-Exam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace B2_Csharp_Dame_Niasse_Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
