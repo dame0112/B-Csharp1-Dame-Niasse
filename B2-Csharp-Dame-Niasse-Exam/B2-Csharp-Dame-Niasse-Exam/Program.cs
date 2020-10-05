033d65b7c6f05d67dc50eb0a8ad3cb40598f4ba7
dame0112 <58723018+dame0112@users.noreply.github.com>
2020-10-05 13:51:36 +0200

    trkl

--- B2-Csharp-Dame-Niasse-Exam/B2-Csharp-Dame-Niasse-Exam/Program.cs
+++ B2-Csharp-Dame-Niasse-Exam/B2-Csharp-Dame-Niasse-Exam/Program.cs
@@ -6,7 +6,7 @@ namespace B2_Csharp_Dame_Niasse_Exam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello world!");
         }
     }
 }
